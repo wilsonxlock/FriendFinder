fa2026d702057d08617e79fcbd01bfd572ff291d
Tyler Wilson <x@tylers-mbp.attlocal.net>
2017-10-03 09:02:50 -0400

    no luck on camparison data, halfway structured but non functional

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,21 @@
 var Profiles = [{
-        Name: "Jonah Boi",
-        Photo: "www.photo.com",
-        Answers: [
+        name: "Jonah Boi",
+        photo: "www.photo.com",
+        answers: [
             1, 5, 3, 1, 2, 2, 4, 2, 5, 4
         ]
     },
     {
-        Name: "Tim Allen",
-        Photo: "www.photo.com",
-        Answers: [
+        name: "Tim Allen",
+        photo: "www.photo.com",
+        answers: [
             5, 3, 4, 1, 2, 2, 3, 5, 4, 1
         ]
     },
     {
-        Name: "ur mom",
-        Photo: "www.photo.com",
-        Answers: [
+        name: "ur mom",
+        photo: "www.photo.com",
+        answers: [
             2, 4, 4, 4, 3, 1, 3, 3, 5, 3
         ]
     }
